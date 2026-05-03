24781e107b3958b6059e62d46db365d660039477
makhairussaadah <madrasahkhairussaadahkubu@gmail.com>
2026-05-03 21:27:54 +0700

    Refactor toggleMenu function and update menu class

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
+// FILE: script.js
+
 function toggleMenu(){
-document.getElementById("navMenu").classList.toggle("active");
+document.getElementById("menu").classList.toggle("show");
 }
-
-document.querySelectorAll(".menu a").forEach(link=>{
-link.addEventListener("click",()=>{
-document.getElementById("navMenu").classList.remove("active");
-});
-});
-
-window.addEventListener("scroll", function(){
-const header = document.querySelector("header");
-
-if(window.scrollY > 20){
-header.style.boxShadow = "0 10px 25px rgba(0,0,0,0.18)";
-}else{
-header.style.boxShadow = "0 8px 20px rgba(0,0,0,0.15)";
-}
-});
