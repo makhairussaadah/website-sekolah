a1414d5f99a9a5ec2245a7e30aa1d05e73359d2c
makhairussaadah <madrasahkhairussaadahkubu@gmail.com>
2026-05-06 22:30:26 +0700

    Add functions to manage brosur popup display

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 function toggleMenu(){
 document.getElementById("menu").classList.toggle("show");
 }
+
+function openBrosur(){
+document.getElementById("brosurPopup").style.display = "flex";
+}
+
+function closeBrosur(){
+document.getElementById("brosurPopup").style.display = "none";
+}
